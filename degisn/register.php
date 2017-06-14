77f17736884a20f6dbb8594158cafd9bf2fcbc6e
Maik Sportel <Maik Sportel>
2017-06-14 16:55:19 +0200

    test1

--- degisn/register.php
+++ degisn/register.php
@@ -16,9 +16,9 @@ include ('menubalk.html');
 
 <section class="bloklogin">
 <form action="register.php" method="post">
-        <input type="text" placeholder="E-mail" name="email" >
-        <input type="password" placeholder="Wachtwoord" name="wachtwoord">
-        <input type="password" placeholder="Herhaal wachtwoord" name="herhaalwachtwoord">
+        <input class="regemail" type="text" placeholder="E-mail" name="email" >
+        <input class="regww" type="password" placeholder="Wachtwoord" name="wachtwoord">
+        <input class="reghhww" type="password" placeholder="Herhaal wachtwoord" name="herhaalwachtwoord">
         <input type="button" class="registreren" value="Registreren">
         <a href="login.php"><input type="button" class="inloggen" value="Login"></a>
 </form>
